a50b99c8b736f020bfda4e169d9fdce86d78d3cc
trinh318 <htttrinh145@gmail.com>
2026-05-21 13:19:18 +0700

    fix PaviLogger

--- torchlight/torchlight/util.py
+++ torchlight/torchlight/util.py
@@ -13,8 +13,10 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
-from torchpack.runner.hooks import PaviLogger
-
+try:
+    from torchpack.runner.hooks import PaviLogger
+except:
+    PaviLogger = None
 
 class IO():
     def __init__(self, work_dir, save_log=True, print_log=True):
